d4761b1157b5c263ec8c593f115c297111be46e5
vishalsingh200 <singhvishal0847@gmail.com>
2026-04-15 00:15:34 +0530

    fix cors issue

--- backend_api.py
+++ backend_api.py
@@ -35,7 +35,11 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://thunderous-lamington-8050fb.netlify.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
